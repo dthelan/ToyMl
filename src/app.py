8c5819a2c03d9a377a1477e3ada0df4a25b719f7
dthelan <dthelan@gmail.com>
2021-03-05 12:30:25 +0000

    Update base.html, create login.html, fix imports on app

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, flash, redirect,url_for
 from joblib import load
 from config import Config
 import pandas as pd
@@ -9,6 +9,8 @@ from process_data import process_training_data
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+
+from forms import LoginForm
 
 
 # Define the app as a flask app
@@ -23,8 +25,48 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 login = LoginManager(app)
+login.login_view = 'login'
 
-import models
+from flask_login import current_user, login_user,logout_user
+from models import User
+
+# Creates an endpoint for login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # Prevent a loged in user from going to login page
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    # Define form to use on page
+    form = LoginForm()
+    # If the form is valid
+    if form.validate_on_submit():
+        # See if there is a valid user name for submitted info
+        user = User.query.filter_by(username=form.username.data).first()
+        # If no user or incorrect password
+        if user is None or not user.check_password(form.password.data):
+            # Flash -> Pop up for invalid password or user
+            flash('Invalid username or password')
+            # Loop back round to login page
+            return redirect(url_for('login'))
+        # If all checks pass register the user as logged on
+        # uses login_user from flask_login
+        login_user(user, remember=form.remember_me.data)
+        # Redirect now logged in user to the index page
+        return redirect(url_for('index'))
+    # Render the login page for new users
+    return render_template('login.html', title='Sign In', form=form)
+
+
+# End point of logging out of the app
+@app.route('/logout')
+def logout():
+    # Use logout_user from flask_login to logout the user
+    logout_user()
+    # Direct the user to index page
+    # This will then redirect to the logon page
+    return redirect(url_for('index'))
+
+
 
 # Define an API end point
 # This is a test to show the page is working
@@ -39,6 +81,7 @@ def index():
 # Use a command like
 # curl --data-binary "@test.csv" --request POST http://localhost:5001/predict
 @app.route('/predict', methods=['GET', 'POST'])
+@login_required
 def prediction():
     # Define different end point for different request types
     # GET - A web page style request
